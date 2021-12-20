84e4d0995d2b51c4064f829ed219e18c2740640a
ouval <dafinnovalarif1@gmail.com>
2021-12-20 22:53:21 +0700

    first commit

---
+++ resources/views/includes/navbar.blade.php
@@ -0,0 +1,83 @@
+<!-- navbar -->
+<div class="container">
+  <nav class="row navbar navbar-expand-lg navbar-light bg-white">
+    <div class="container">
+  
+      <a class="navbar-brand" href="{{ url('/') }}">
+        <img src="{{ url('frontend/images/logo.png') }}" alt="" />
+      </a>
+      <button
+        class="navbar-toggler navbar-toggler-right"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navb"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+
+      <!-- Menu -->
+      <div class="collapse navbar-collapse" id="navb">
+        <ul class="navbar-nav ms-auto me-3">
+          <li class="nav-item mx-md-2">
+            <a class="nav-link active" href="#">Home</a>
+          </li>
+          <li class="nav-item mx-md-2">
+            <a class="nav-link" href="#">Paket Travel</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbardrop"
+              role="button"
+              data-bs-toggle="dropdown"
+            >
+              Services
+            </a>
+            <ul class="dropdown-menu">
+              <a class="dropdown-item" href="#">Link 1</a>
+              <a class="dropdown-item" href="#">Link 2</a>
+              <a class="dropdown-item" href="#">Link 3</a>
+            </ul>
+          </li>
+          <li class="nav-item mx-md-2">
+            <a class="nav-link" href="#">Testimonial</a>
+          </li>
+        </ul>
+        @guest
+            <!-- Mobile button -->
+          <form class="form-inline d-sm-block d-md-none">
+            <button class="btn btn-login my-2 my-sm-0" type="button" 
+              onclick="event.preventDefault(); location.href='{{ url('login') }}';">
+              Masuk
+            </button>
+          </form>
+            <!-- Desktop Button -->
+          <form class="form-inline my-2 my-lg-0 d-none d-md-block">
+            <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4" type="button" 
+              onclick="event.preventDefault(); location.href='{{ url('login') }}';">
+              Masuk
+            </button>
+          </form>   
+        @endguest
+
+        @auth
+            <!-- Mobile button -->
+          <form class="form-inline d-sm-block d-md-none" action="{{ url('logout') }}" method="POST">
+            @csrf
+            <button class="btn btn-login my-2 my-sm-0" type="submit">
+              Keluar
+            </button>
+          </form>
+            <!-- Desktop Button -->
+          <form class="form-inline my-2 my-lg-0 d-none d-md-block" action="{{ url('logout') }}" method="POST">
+            @csrf
+            <button class="btn btn-login btn-navbar-right my-2 my-sm-0 px-4" type="submit">
+              Keluar
+            </button>
+          </form>   
+        @endauth
+      </div>
+    </div>
+  </nav>  
+</div>
---
+++ resources/views/includes/script.blade.php
@@ -0,0 +1,3 @@
+<script src="{{ url('frontend/libraries/retina/retina.min.js') }}"></script>
+<script src="https://code.jquery.com/jquery-3.6.0.slim.js" integrity="sha256-HwWONEZrpuoh951cQD1ov2HUK5zA5DwJ1DNUXaM6FsY=" crossorigin="anonymous"></script>
+<script src="{{ url('frontend/libraries/bootstrap/js/bootstrap.js') }}"></script>
